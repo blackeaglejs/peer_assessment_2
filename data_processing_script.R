9cdd0ddb5a55dc63155b16b8f7327264ebb8b085
Zoheb Nensey <zoheb.nensey@gmail.com>
2015-09-26 18:20:38 -0400

    Add results for question 1 from assignment. Still need plot.

--- data_processing_script.R
+++ data_processing_script.R
@@ -15,3 +15,22 @@ download.file(fileUrl, "storm_data.csv.bz2", method="curl")
 ## Read in the data. 
 storm_data <- read.csv("storm_data.csv.bz2")
 
+## Use dplyr to group the data. 
+storm_data_by_type <- storm_data %>%
+      group_by(EVTYPE) %>%
+      summarize(fatalities_sum=sum(FATALITIES),
+                injury_sum=sum(INJURIES),
+                total_pop_health = fatalities_sum+injury_sum)
+
+## Sort the grouped data 
+storm_data_type_sorted <- storm_data_by_type[order(-storm_data_by_type$total_pop_health),]
+
+
+## Get results for the worst kind of events. 
+biggest_event <- head(storm_data_type_sorted$EVTYPE, n=1)
+event_fatalities <- head(storm_data_type_sorted$fatalities_sum,n=1)
+event_injuries <- head(storm_data_type_sorted$injury_sum,n=1)
+event_total <- head(storm_data_type_sorted$total_pop_health,n=1)
+
+## Created a barplot for the events. 
+barplot(storm_data_type_sorted$total_pop_health)
